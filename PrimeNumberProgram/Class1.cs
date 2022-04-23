1b7ac33d8137fd880056fa427e68e7b1f1be3a1f
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-24 02:21:05 +0530

    Add project files.

---
+++ PrimeNumberProgram/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberProgram
+{
+    internal class Class1
+    {
+        public static void prime()
+        {
+            int count = 0;
+            Console.WriteLine("Enter a number:");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Number is Not Prime Number");
+                    count = 1;
+                    break;
+                }
+
+            }
+
+
+            if (count == 0)
+            {
+                Console.WriteLine("Number is Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
